0636150cda67500efded3e1d70c8e6cec384705a
viviiaura <victoriaau23@gmail.com>
2026-08-18 19:58:15 +0000

    rectanglee

--- rectangle.py
+++ rectangle.py
@@ -1,11 +1,21 @@
 def main():
 
+    l = 5
     w = int(input("What is the width? "))
     print("0" * w)
     print("0" * w)
     print("0" * w)
     print("0" * w)
     print("0" * w)
+
+    p = (2 * l) + (2 * w)
+    print("Perimeter:", p)
+
+    a = (l * w)
+    print("area:", a)
+
+    d = (l ** 2 + w ** 2) ** 1/2
+    print("Diagonal:", d)
 
 if __name__ == "__main__":
     main()
