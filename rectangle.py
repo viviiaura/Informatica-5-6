b56a06214e9116e25147a1ce18f57e2240560ba1
viviiaura <victoriaau23@gmail.com>
2026-08-18 19:46:14 +0000

    rectangle

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,11 @@
 def main():
 
     w = int(input("What is the width? "))
-    p = print("0" * w)
+    print("0" * w)
+    print("0" * w)
+    print("0" * w)
+    print("0" * w)
+    print("0" * w)
 
 if __name__ == "__main__":
     main()
