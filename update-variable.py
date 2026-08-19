46d3c7edeb3a791423807d1a07b0a659165fc477
viviiaura <victoriaau23@gmail.com>
2026-08-19 18:00:34 +0000

    update variable

---
+++ update-variable.py
@@ -0,0 +1,36 @@
+def main():
+    # Augmented Assignment Opetator
+    score = 10
+    score += 10
+    print(score)
+
+    # Substraction
+
+    # "x-=y"
+    grade = 100
+    grade -= 15
+    print(grade)
+
+    # Multiplication Assignment Operator
+    balance = 20
+    balance *= 4
+    print(balance)
+
+    #Division Assignment Operator
+
+    x = 10
+    x /= 5
+
+    print(x)
+
+    #Modulus Assignment Operator
+
+    a = 10
+    b = 4
+
+    a %= b
+    print(a)
+
+
+if __name__== "__main__":
+    main()
