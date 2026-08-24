8a60696bac325bdaa1add13335300b7f53d341af
viviiaura <victoriaau23@gmail.com>
2026-08-24 14:52:34 +0000

    passs

---
+++ password.py
@@ -0,0 +1,13 @@
+def main():
+
+    password = "HackingIntoTheMainframe5000"
+    attempt = input("Whats the password? ")
+
+    if attempt == password:
+        print("yay you remember 🥳")
+    else:
+        print("blowing up computer...")
+
+
+if __name__ == "__main__":
+    main()
