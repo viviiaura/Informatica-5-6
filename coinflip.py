83c26ffa87c7525e76b42f473ad9b3e2dfa27dc0
viviiaura <victoriaau23@gmail.com>
2026-09-01 20:19:12 +0000

    coin

---
+++ coinflip.py
@@ -0,0 +1,18 @@
+import random
+
+def main():
+
+    guess = print("Heads or tails?: ").lower()
+    outcome = random.ranint(1,2)
+
+    if outcome == 1:
+        if guess == heads
+        print("Your guess was heads, the coin is heads)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
