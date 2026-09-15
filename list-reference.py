0dedfaa1fa9a32abc8e92200fb186d3061c9be77
viviiaura <victoriaau23@gmail.com>
2026-09-15 15:50:22 +0000

    radio

--- list-reference.py
+++ list-reference.py
@@ -14,5 +14,31 @@ def main():
     print(max(numbers))
     print(sum(numbers))
 
+    x = len("hello")
+
+    print(x)
+    password = [1, 2, 3, 4, 5, 6, 7, 8]
+    xx = ["a", "b", "c", "d"]
+    print(len(xx))
+    print(password)
+    change = input(" ")
+
+    if change == "y":
+        password[1]= 4
+        password[0] = 2
+        password[7] = 1
+        password[6] = 9
+        password[5] = 10
+        print(password)
+
+    numbers = [10, 2, 5, 3, 4, 8, 7]
+    print(min(numbers))
+    print(max(numbers))
+    print(sum(numbers))
+
+    tacos = ["Pastor", "Asada", "Suadero", "Rajas", "Papa", "Dorados", "Buche"]
+    print(tacos)
+    print(tacos.pop(3))
+    print(tacos)
 if __name__ == "__main__":
     main()
---
+++ radio-playlist.py
@@ -0,0 +1,28 @@
+import time
+
+def main():
+    playlist = ["Boston", "Dracula", "I Knew It, I Knew You", "hate that i made you love me", "Risk It All"]
+    playlist.append("Be By You")
+
+    playlist.insert(0, "Bohemian Rhapsody")
+
+    playlist.pop(4)
+    print(playlist.index("Risk It All"))
+    print("Number of songs in playlist:", len(playlist))
+    playlist.reverse()
+    print(playlist)
+    playlist.sort()
+    print(playlist)
+
+    # Challenge
+    repeat = len(playlist)
+    while repeat > 0:
+        print(playlist)
+        song = playlist[0]
+        playlist.pop(0)
+        playlist.append(song)
+        repeat -= 1
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main()
