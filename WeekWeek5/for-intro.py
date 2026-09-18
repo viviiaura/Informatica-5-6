c57959d7313236e604cc844b46a9f7e6d05eb774
viviiaura <victoriaau23@gmail.com>
2026-09-18 20:20:44 +0000

    hihi

---
+++ WeekWeek5/for-intro.py
@@ -0,0 +1,10 @@
+def main():
+    doctrine = ["Faith in Jesus Christ", "Repentance", "Baptism in water", "Confermation", "Enduring the end"]
+
+    list = 1
+    for hi in doctrine:
+        print(f"{list}. {hi}")
+        list += 1
+
+if __name__ == "__main__":
+    main()
