e784881a9cdf2a05c88f7d6c043a4fcf64290e05
viviiaura <victoriaau23@gmail.com>
2026-09-23 17:40:37 +0000

    tablesss

--- tables.py
+++ tables.py
@@ -1,16 +1,25 @@
 def main():
 
-    answer = int(input("Give me a number 1-10 "))
     tables = [1,2,3,4,5,6,7,8,9,10]
 
-    if answer <= 10:
-        print(f"Here is the {answer} times table")
-        for i in range(1,11):
+    while True:
+        answer = int(input("Give me a number 1-10 "))
+        if answer <= 10:
+            print(f"Here is the {answer} times table")
+            for i in range(1,11):
 
-            result = i * answer
-            print(f"{i} times {answer} is {result}")
-    elif answer > 10:
-        print("Kay so i said 1-10")
+                result = i * answer
+                print(f"{i} times {answer} is {result}")
+            another = input("Do you want another one? ").lower().strip()
+            if another == "yes":
+                print(answer)
+            elif another == "no":
+                break
+
+        elif answer > 10:
+            print("kay so I said 1-10")
+
+
 
 
 if __name__ == "__main__":
